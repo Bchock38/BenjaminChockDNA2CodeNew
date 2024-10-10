1c1b40b25693780badf1a8a083694db8228ed69b
Benjamin Chock <benjamin.chock@menloschool.org>
2024-10-10 14:51:16 -0700

    commit 1

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date       |   Time    |                                                                                            Update |
+|:-----------|:---------:|--------------------------------------------------------------------------------------------------:|
+| April 5    |  9-10pm   |                                                        I tried to write abstract, efficient code. |
+| October 10 | 2-2:40 pm | Finished making a protype intarative way of solving the problem set, but it's very very very slow |
+|            |           |                                                                                                   |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/DNA.java
+++ src/DNA.java
@@ -12,11 +12,38 @@
 
 public class DNA {
 
+
     /**
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        int StrCount = 0;
+        return StrCount;
+    }
 
-        return 0;
+    public int STRCountInterative(String sequence, String STR){
+        int StrCount = 0;
+        int biggest = 0;
+        String temp_STR;
+        String temp_STR_old = "";
+        while (sequence.length() > STR.length()){
+            temp_STR = sequence.substring(0,STR.length());
+            if (temp_STR.equals(STR)){
+                temp_STR_old = temp_STR;
+            }
+            if (temp_STR.equals(temp_STR_old)){
+                biggest++;
+                sequence = sequence.substring(STR.length());
+            }
+            else{
+                if (biggest > StrCount){
+                    StrCount = biggest;
+                }
+                biggest = 0;
+                sequence = sequence.substring(1);
+            }
+
+        }
+        return StrCount;
     }
 }
